112ddf500d67a470033738a6212d3e8540f93890
heejae_shin <heejae_shin@tmax.co.kr>
2022-05-11 14:48:57 +0900

    OwingTest added

--- src/extract_method/Owing.java
+++ src/extract_method/Owing.java
@@ -14,7 +14,10 @@ public class Owing {
     void printOwing() {
         Enumeration e = _orders.elements();
         double outstanding = 0.0;
-        printBanner();
+        // print banner
+        System.out.println ("**************************");
+        System.out.println ("***** Customer Owes ******");
+        System.out.println ("**************************");
         // calculate outstanding
         while (e.hasMoreElements()) {
             Order each = (Order) e.nextElement();
@@ -26,10 +29,18 @@ public class Owing {
     }
 
     void printBanner() {
-        // print banner
-        System.out.println ("**************************");
-        System.out.println ("***** Customer Owes ******");
-        System.out.println ("**************************");
+
+    }
+    void printDetails(double outstanding) {
+
     }
 
+    public static void main(String[] args) {
+        Owing owing = new Owing("o1");
+        owing._orders.add(new Order(10));
+        owing._orders.add(new Order(20));
+        owing._orders.add(new Order(30));
+        owing._orders.add(new Order(50));
+        owing.printOwing();
+    }
 }
--- src/extract_method/OwingTest.java
+++ src/extract_method/OwingTest.java
@@ -1,5 +1,9 @@
 package extract_method;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwingTest {
@@ -8,23 +12,38 @@ class OwingTest {
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         owing = new Owing("o1");
+        owing._orders.add(new Order(10));
+        owing._orders.add(new Order(20));
+        owing._orders.add(new Order(30));
+        owing._orders.add(new Order(50));
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
     }
 
-//    @org.junit.jupiter.api.Test
-//    void printOwing() {
-//        owing._orders.add(new Order(10));
-//        owing._orders.add(new Order(20));
-//        owing._orders.add(new Order(30));
-//        owing._orders.add(new Order(50));
-//        assertEquals("**************************\n"
-//                + "***** Customer Owes ******\n"
-//                + "**************************\n"
-//                + "name:" + "o1\n"
-//                + "amount" + 110,
-//                );
-//    }
+    @org.junit.jupiter.api.Test
+    void printOwing() {
+        // Create a stream to hold the output
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+// IMPORTANT: Save the old System.out!
+        PrintStream old = System.out;
+// Tell Java to use your special stream
+        System.setOut(ps);
+// Print some output: goes to your special stream
+        owing.printOwing();
+// Put things back
+        System.out.flush();
+        System.setOut(old);
+
+        assertEquals("**************************\n" +
+                        "***** Customer Owes ******\n" +
+                        "**************************\n" +
+                        "name:o1\n" +
+                        "amount110.0\n",
+                baos.toString().replaceAll("\\r\\n?", "\n")
+                , "printOwing message fails"
+        );
+    }
 }
